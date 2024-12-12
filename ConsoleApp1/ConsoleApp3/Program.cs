1c79c947fc0e22993fcbda054b04eae8f27f4c59
krenddl <wf.gabit@mail.ru>
2024-12-12 13:38:17 +0300

    practic case

---
+++ ConsoleApp1/ConsoleApp3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int number = int.Parse(Console.ReadLine());
+
+            try
+            {
+                if (number < 0)
+                {
+                    throw new Exception("Ввод отрицательного числа");
+                }
+
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp4/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] stock = { 10, 10, 10 };
+            int[] sales = {0, 0, 0 };
+            int[] price = { 100, 150, 200 };
+            int count = 0;
+            int count2 = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Добро пожаловать в кофейню мечты!\r\n Выберите действие:\r\n1. Обработать заказ\r\n2. Просмотреть статисику продаж\r\n3. Проверить запасы на складе\r\n4. Заверишть работу ");
+                Console.WriteLine();
+                string operation = Console.ReadLine();
+                switch (operation)
+                    {
+                    case "1":
+                        {
+                            Console.WriteLine("Меню напитков:\r\n1. Эспрессо - 100 руб.\r\n2. Капучино - 150 руб.\r\n3. Латте - 200 руб.");
+                            string[] menu = {"Эспрессо", "Капучино", "Латте" };
+                            int drink = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Введите номер напитка: " + drink);
+                            if (stock[drink-1] > 0)
+                            {
+                                Console.WriteLine($"Вы заказали: {menu[drink-1]}. Приятного аппетита");
+                                stock[drink-1]--;
+                                sales[drink-1]++;
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Извините, но {menu[drink-1]} закончился");
+                            }
+                            break;
+                        }
+                    case "2":
+                        {
+                            count = (100*sales[0]) + (150 * sales[1]) + (200 * sales[2]);
+                            Console.WriteLine($"Продано напитков в общем: {count}\r\n Эспрессо - {sales[0]*100}\r\n Капучино - {sales[1]*150}\r\n Латте - {sales[2]*200}");
+                            break;
+                        }
+                    case "3":
+                        {
+                            Console.WriteLine($"Запасы на складе: \r\n Эспрессо: {stock[0]} в наличии \r\n Капучино: {stock[1]}в наличии \r\n Латте: {stock[2]} в наличии");
+                            break;
+                        }
+                    case "4":
+                        {
+                            return;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Неправильный ввод");
+                            break;
+                        }
+                    }
+            }
+
+        }
+
+    }
+}
